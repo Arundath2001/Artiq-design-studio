abd651058d89c1ae0ad943f01138a7cc6c8d3782
Arundath2001 <arundatharun44@gmail.com>
2024-10-10 15:27:56 +0530

    latest

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './Work.css';
 import WorkCard from "./WorkCard";
-import workimg1 from "../assets/workimg1.png"
-import workimg2 from "../assets/workimg2.png"
-import workimg3 from "../assets/workimg3.png"
+import workimg1 from "../assets/Bhoomika.jpeg"
+import workimg2 from "../assets/darkroom.jpeg"
+import workimg3 from "../assets/egrocery.jpeg"
 import workimg4 from "../assets/workimg4.gif"
-import workimg5 from "../assets/workimg5.png"
-import workimg6 from "../assets/workimg6.png"
+import workimg5 from "../assets/shyamjith.jpeg"
+import workimg6 from "../assets/wedding.jpeg"
 
 function Work(){
 
